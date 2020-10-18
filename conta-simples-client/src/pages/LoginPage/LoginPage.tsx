a33727ad388ee9e0c961263de90207fdc5e89bc2
Hiaguedes <hiago_guedes94@hotmail.com>
2020-10-18 15:32:24 -0300

    implementando validação básica

--- conta-simples-client/src/pages/LoginPage/LoginPage.tsx
+++ conta-simples-client/src/pages/LoginPage/LoginPage.tsx
@@ -19,22 +19,29 @@ function LoginPage() {
     const history = useHistory();
     const [object, setObject] = useState<ObjectLogin[]>([]);
     const [inputText,setInputText] = useState<String>('');
+    const [errorInput,setErrorInput] = useState(false);
 
     function handleFormData(){
         for(let i =0; i<object.length;i++){
             if(object[i].cnpj === inputText.replace(/\D/g,'')) {
-                console.log(i)
               history.push(`/empresa/${i+1}`)
             }
         }
-        return null;
+        setErrorInput(true);
     }
 
     return (
         <main className="login_container">
             <form  className="login_form">
                 <h2 className="login_title">Login</h2>
-                <input onChange={e => setInputText(e.target.value)} className="login_input" type="text" placeholder="Coloque o CNPJ"/>
+                <div className="login-input_container">
+                    <input onChange={e => setInputText(e.target.value)} className={`login_input  ${(errorInput ? "login_input--erro" : "")}`}  type="text" placeholder="Coloque o CNPJ"/>
+                    {
+                    errorInput ? 
+                    (<div className="erro_message">Alguma Coisa Está Errada</div>): 
+                    (<></>)
+                    }
+                </div>
                 <button onClick={handleFormData} className="login_button" type="button">Entrar na conta</button>
             </form>
         </main>
