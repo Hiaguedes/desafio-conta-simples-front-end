75810ef9fde389480b666d0330add1436657a510
Hiaguedes <hiago_guedes94@hotmail.com>
2020-10-19 15:34:21 -0300

    colocando grafico de linha na pagina da empresa

--- conta-simples-client/src/pages/CompanyProfile/CompanyProfile.tsx
+++ conta-simples-client/src/pages/CompanyProfile/CompanyProfile.tsx
@@ -3,6 +3,7 @@ import './CompanyProfile.css';
 import api from '../../services/api';
 import {useParams} from 'react-router-dom';
 import Sidebar from '../../components/Sidebar/Sidebar';
+import {Chart} from 'react-charts';
 
 interface Params {
     id: string;
@@ -30,6 +31,72 @@ function CompanyProfile(){
         .then(res => setCompanyInfo(res.data)
         )},[id])
 
+        const data = [
+            {
+              // individual series
+              label: "Entrada",
+              // datum array
+              data: [
+                {
+                  // individual datum
+                  primary: new Date(2011,10,1), // primary value
+                  secondary: 20 // secondary value
+                },
+                {
+                  // individual datum
+                  primary: new Date(2011,10,2), // primary value
+                  secondary: 250 // secondary value
+                },
+                {
+                  // individual datum
+                  primary: new Date(2011,10,2,8), // primary value
+                  secondary: 250 // secondary value
+                }
+              ]
+            },
+            {
+              label: "Saída",
+              data: [
+                {
+                  // individual datum
+                  primary: new Date(2011,10,1), // primary value
+                  secondary: 50 // secondary value
+                },
+                {
+                  // individual datum
+                  primary: new Date(2011,10,2), // primary value
+                  secondary: 20 // secondary value
+                },
+                {
+                  // individual datum
+                  primary: new Date(2011,10,3), // primary value
+                  secondary: 130 // secondary value
+                }
+              ]
+            }
+          ];
+        
+          const series = React.useMemo(
+            () => ({
+              showPoints: false
+            }),
+            []
+          );
+        
+          const axes = React.useMemo(
+            () => [
+              {
+                primary: true,
+                type: "utc",
+                position: "bottom"
+                // filterTicks: (ticks) =>
+                //   ticks.filter((date) => +timeDay.floor(date) === +date),
+              },
+              { type: "linear", position: "left" }
+            ],
+            []
+          );
+
     if(!companyInfo) return <p>Empresa não encontrada error 404</p>
     return(
         <main className="company-profile">
@@ -43,6 +110,9 @@ function CompanyProfile(){
                         <p>Conta: {companyInfo[0].dadosBancario.conta}</p>
                         <p>Digito Conta: {companyInfo[0].dadosBancario.digitoConta}</p>
                 </details>
+                <div className="chart">
+                    <Chart  data={data} series={series} axes={axes} tooltip/>
+                </div>
             </div>
         </main>
     )
