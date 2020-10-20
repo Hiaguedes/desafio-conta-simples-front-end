2dd709f930775718ca16ced01114a47a3cb532ce
Hiaguedes <hiago_guedes94@hotmail.com>
2020-10-20 20:07:10 -0300

    adicionando tabela de extrato

--- conta-simples-client/src/pages/ExtratoPage/ExtratoPage.tsx
+++ conta-simples-client/src/pages/ExtratoPage/ExtratoPage.tsx
@@ -22,6 +22,7 @@ interface CompanyTransactions {
     finalCartao: number;
     tipoTransacao: string;
     descricaoTransacao: string;
+    estabelecimento: string;
     credito: boolean;
 }
 
@@ -54,7 +55,36 @@ export default function ExtratoPage() {
             <Sidebar nomeEmpresa={companyInfo[0].nomeEmpresa} saldo={companyInfo[0].saldo}/>
             <div className="extract-page_container">
                 <h2 className="extract-page_title">Extrato da empresa</h2>
-
+                <div className="table_container">
+                <table className="table">
+                    <thead className="table_header">
+                        <tr>
+                        <th className="table_header-data">Data</th>
+                        <th className="table_header-data">Horário</th>
+                        <th className="table_header-data">Descricao</th>
+                        <th className="table_header-data">Estabelecimento</th>
+                        <th className="table_header-data">Valor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {companyTransactions.map((transaction,index) =>{
+                            const arrayDataHorario = transaction.dataTransacao.replace('T',' ').split(' ');
+                            const arrayDia = arrayDataHorario[0].split('-');
+                            return(
+                                
+                                    <tr key={index} className={`table_body-line ${(transaction.tipoTransacao === 'SLIP_IN' ||transaction.tipoTransacao === 'TED_IN')? 'entry' : 'exit'}`}>
+                                        <td className="table_body-data">{`${arrayDia[2]}/${arrayDia[1]}/${arrayDia[0]}`}</td>
+                                        <td className="table_body-data">{arrayDataHorario[1]}</td>
+                                        <td className="table_body-data">{transaction.descricaoTransacao}</td>
+                                        <td className="table_body-data">{transaction.estabelecimento}</td>
+                                        <td className="table_body-data">{`R$ ${transaction.valor.toFixed(2)}`}</td>
+                                    </tr>
+                                
+                            )
+                        })}
+                </tbody>
+                </table>
+                </div>
             </div>
         </main>
     )
