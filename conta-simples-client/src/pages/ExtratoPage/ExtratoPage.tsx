44eed095e15852995d26bc3042cd0e1c7dd1ef45
Hiaguedes <hiago_guedes94@hotmail.com>
2020-10-21 00:32:26 -0300

    fazendo filtragem na tabela com o tipo da transação e por credito ou débito

--- conta-simples-client/src/pages/ExtratoPage/ExtratoPage.tsx
+++ conta-simples-client/src/pages/ExtratoPage/ExtratoPage.tsx
@@ -38,6 +38,8 @@ export default function ExtratoPage() {
     const [companyInfo, setCompanyInfo] = useState<CompanyInfo>()
     const [companyTransactions, setCompanyTransactions] = useState<CompanyTransactions[]>([]);
     const [buttonClicked,setButtonClicked] = useState(-1);
+    const [selectTransactionFilter,setSelectTransactionFilter] = useState('any');
+    const [selectPaymentFilter,setSelectPaymentFilter] = useState('any');
 
     useEffect(()=>{
         api.get(`empresas/${id}`)
@@ -54,13 +56,45 @@ export default function ExtratoPage() {
 
     }
 
-    if(!companyInfo) return <p>Empresa não encontrada erro 404</p>
+    function handleTransactionType(e: React.ChangeEvent<HTMLSelectElement>){
+        setSelectTransactionFilter(e.target.value);
+    }
 
+    function handlePaymentFilter(e: React.ChangeEvent<HTMLSelectElement>){
+        setSelectPaymentFilter(e.target.value);
+    }
+
+    if(!companyInfo) return <p>Empresa não encontrada erro 404</p>
+    //console.log(selectTransactionFilter,selectPaymentFilter)
     return(
         <main className="extract-page">
             <Sidebar nomeEmpresa={companyInfo[0].nomeEmpresa} saldo={companyInfo[0].saldo}/>
             <div className="extract-page_container">
                 <h2 className="extract-page_title">Extrato da empresa</h2>
+                <div className="filtros_container">
+                    <h3>Filtros para a Tabela</h3>
+                    <div className="filtro">
+
+                    <div>
+                    <p>Tipo da Transação: </p>
+                    <select onChange={(e) => handleTransactionType(e)}>
+                        <option value="any">Qualquer</option>
+                        <option value="TED_IN">TED IN</option> {/*Desculpa a ignorância mas não sei mesmo o que seria TED IN ou SLIP IN hahaha (muito menos o SLIP e como não achei nada na internet então sorry*/}
+                        <option value="SLIP_IN">SLIP IN</option>
+                        <option value="CARD">CARD</option>
+                        <option value="PAY">PAY</option>
+                    </select>
+                    </div>
+                    <div>
+                        <p>Crédito ou Débito?</p>
+                    <select onChange={(e) => handlePaymentFilter(e)}>
+                        <option value="any">Qualquer</option>
+                        <option value="true">Crédito</option> {/*Desculpa a ignorância mas não sei mesmo o que seria TED IN ou SLIP IN hahaha (muito menos o SLIP e como não achei nada na internet então sorry*/}
+                        <option value="false">Débito</option>
+                    </select>
+                    </div>
+                    </div>
+                </div>
                 <div className="table_container">
                 <table className="table">
                     <thead className="table_header">
@@ -74,7 +108,20 @@ export default function ExtratoPage() {
                         </tr>
                     </thead>
                     <tbody>
-                        {companyTransactions.map((transaction,index) =>{
+                        {companyTransactions
+                            .filter(transaction =>{
+                                if(selectTransactionFilter !== 'any')
+                                    return transaction.tipoTransacao === `${selectTransactionFilter}`
+
+                                return transaction.tipoTransacao !== null
+                            })
+                            .filter(transaction => {
+                                if(selectPaymentFilter !== 'any')
+                                return String(transaction.credito) === selectPaymentFilter
+
+                                return transaction.credito !== null
+                            })
+                            .map((transaction,index) =>{
                             const arrayDataHorario = transaction.dataTransacao.replace('T',' ').split(' '); // divido a string em dois arrays, a primeira contem o dia a segunda o horário
                             const arrayDia = arrayDataHorario[0].split('-');
                             return(
@@ -97,6 +144,10 @@ export default function ExtratoPage() {
                                                {transaction.estabelecimento && <p>Nome do Estabelecimento Beneficiado: {transaction.estabelecimento}</p>}
                                                {transaction.finalCartao && <p>Número final do Cartão: {transaction.finalCartao}</p>}
                                                <p>Tipo da Transação: {transaction.tipoTransacao}</p>
+                                               {transaction.credito? 
+                                               <p>Pagamento feito no crédito</p>
+                                                : <p>Pagamento feito no débito</p>
+                                            }
                                                 
                                                 </td>)
                                             :
